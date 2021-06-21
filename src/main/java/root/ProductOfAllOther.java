09375eb6faa8f5914b006e5ef28e148fbee1e244
samehadel <eng.sameh95@outlook.com>
2021-06-21 15:42:20 +0200

    ProductOfAllOther Problem Added

---
+++ src/main/java/root/ProductOfAllOther.java
@@ -0,0 +1,42 @@
+package root;
+
+/*
+    Problem:
+    You have an array of integers, and for each index you want to find the product of every integer
+     except the integer at that index. Write a method getProductsOfAllIntsExceptAtIndex()
+     that takes an array of integers and returns an array of the products.
+
+     For example, given: [1, 7, 3, 4]
+     your method would return: [84, 12, 28, 21]
+
+ */
+public class ProductOfAllOther {
+    public static int[] getProductsOfAllIntsExceptAtIndex(int[] intArray){
+        int [] beforeIndex = new int [intArray.length];
+        int [] afterIndex = new int [intArray.length];
+        int [] result = new int [intArray.length];
+
+        for(int i = 0; i < intArray.length; i++){
+            if(i == 0) beforeIndex[0] = intArray[0];
+            else
+                beforeIndex[i] = beforeIndex[i - 1] * intArray[i];
+        }
+
+        for(int i = intArray.length - 1; i >= 0; i--){
+            if(i == intArray.length - 1)
+                afterIndex[intArray.length - 1] = intArray[intArray.length - 1];
+            else
+                afterIndex[i] = afterIndex[i + 1] * intArray[i];
+        }
+
+        //Do the multiplication
+        result[0] = afterIndex[1];
+        result[intArray.length - 1] = beforeIndex[intArray.length - 2];
+
+        for(int i = 1; i < intArray.length - 1; i++){
+            result[i] = beforeIndex[i - 1] * afterIndex[i + 1];
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/root/ProductOfAllOtherTest.java
@@ -0,0 +1,33 @@
+package root;
+
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class ProductOfAllOtherTest {
+
+    public static Collection initData(){
+        return Arrays.asList(
+                Arguments.of(new int[]{1, 2, 6, 5, 9}, new int []{540, 270, 90, 108, 60}),
+                Arguments.of(new int[]{8, 2, 4, 3, 1, 5}, new int []{120, 480, 240, 320, 960, 192}),
+                Arguments.of(new int[]{6, 2, 0, 3}, new int []{0, 0, 36, 0}),
+                Arguments.of(new int[]{4, 0, 9, 1, 0}, new int []{0, 0, 0, 0, 0}),
+                Arguments.of(new int[]{-3, 8, 4}, new int []{32, -12, -24}),
+                Arguments.of(new int[]{-7, -1, -4, -2}, new int []{-8, -56, -14, -28})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("initData")
+    public void checkCafeOrder(int [] intArray, int [] expected){
+        int [] actual = ProductOfAllOther.getProductsOfAllIntsExceptAtIndex(intArray);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+}
